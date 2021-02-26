017dc533ce46ce54f9b1b0c7b29f4a7e3bbc0db3
Kiattisak Dujda <kiattisak.dujda@gmail.com>
2021-02-26 13:45:09 +0000

    working on contact from

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav-links");
 const navLinks = document.querySelectorAll(".nav-links li");
 
+// const contactForm = document.querySelector("#contact-form");
+const contactForm = document.forms[0];
+
+// const contactInfo = document.querySelectorAll(
+//   "#contact-form input, #contact-form textarea"
+// );
+
 const navSlide = () => {
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
@@ -31,4 +38,52 @@ const navSlide = () => {
   });
 };
 
-navSlide();
+const clearForm = () => {
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const message = document.getElementById("message");
+
+  name.value = "";
+  email.value = "";
+  message.value = "";
+};
+
+const capitalize = (s) => {
+  if (typeof s !== "string") return "";
+  return s.charAt(0).toUpperCase() + s.slice(1);
+};
+
+// Get contact detail from contact form
+const formSubmit = () => {
+  const messagePosition = document.querySelector(".three");
+
+  contactForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // console.log(event.target.elements);
+    // const contactInfo = document.querySelectorAll(
+    //   "#contact-form input, #contact-form textarea"
+    // );
+
+    const { name, email, message } = event.target.elements;
+    const info = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    // info.name = name.value;
+    // info.email = email.value;
+    // info.message = message.value;
+    const textInfo = `<p class="message">Thank you for your message, ${capitalize(
+      info.name
+    )}. We will be in touch soon.</p>`;
+    clearForm();
+    messagePosition.insertAdjacentHTML("beforeend", textInfo);
+  });
+};
+
+const start = () => {
+  navSlide();
+  formSubmit();
+};
+
+start();
